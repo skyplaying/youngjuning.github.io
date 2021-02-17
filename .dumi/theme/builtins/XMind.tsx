d505d0cd447b2c16f2d9f9db3dd3c751f62e9717
youngjuning <youngjuning@163.com>
2021-02-17 17:34:22 +0800

    样式移到组件内

--- .dumi/theme/builtins/XMind.tsx
+++ .dumi/theme/builtins/XMind.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default (props: {id: string}) => {
-  return (<iframe src={`https://www.xmind.net/embed/${props.id}`} width="100%" height="580px"></iframe>)
+  return (<iframe src={`https://www.xmind.net/embed/${props.id}`} width="100%" height="580px" style={{borderColor: "#f4de50", borderStyle:"double"}}></iframe>)
 }
