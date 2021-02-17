6e581c0b1a5dea82ea3757d537621a9c52dce6f3
youngjuning <youngjuning@163.com>
2021-02-18 06:34:15 +0800

    注释掉评论

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react';
 import type { IRouteComponentProps } from '@umijs/types';
 import { context, Link } from 'dumi/theme';
-import 'gitalk/dist/gitalk.css';
-import GitalkComponent from 'gitalk/dist/gitalk-component';
+// import 'gitalk/dist/gitalk.css';
+// import GitalkComponent from 'gitalk/dist/gitalk-component';
 import Badge from './builtins/Badge'
 import Alert from './builtins/Alert'
 import Navbar from './components/Navbar';
@@ -102,7 +102,7 @@ const Layout: React.FC<IRouteComponentProps> = ({ children, location }) => {
           <img src={meta.cover} width="70%"/>
         </div>
         {children}
-        {!showHero && !showFeatures && meta.filePath && !meta.gapless && (
+        {/* {!showHero && !showFeatures && meta.filePath && !meta.gapless && (
           <div className="__dumi-default-layout-comment">
             <GitalkComponent
               options={{
@@ -115,7 +115,7 @@ const Layout: React.FC<IRouteComponentProps> = ({ children, location }) => {
               }}
             />
           </div>
-        )}
+        )} */}
         {!showHero && !showFeatures && meta.filePath && !meta.gapless && (
           <div className="__dumi-default-layout-footer-meta">
             {repoPlatform && (
