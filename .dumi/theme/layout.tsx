84127b669236542d77a0524d88eac19a73b3c359
洛竹 <youngjuning@aliyun.com>
2021-07-15 20:12:40 +0800

    将初始化的动作放在外面

--- .dumi/theme/layout.tsx
+++ .dumi/theme/layout.tsx
@@ -54,10 +54,10 @@ const Layout: React.FC<IRouteComponentProps> = ({ children, location }) => {
   } = useContext(context);
 
   useEffect(() => {
+    const container = document.getElementById('gitalk-container');
+    ReactDOM.unmountComponentAtNode(container);
+    container.innerHTML = '';
     if (!showHero && !showFeatures) {
-      const container = document.getElementById('gitalk-container');
-      ReactDOM.unmountComponentAtNode(container);
-      container.innerHTML = '';
       const gitalk = new Gitalk({
         clientID: 'a6e0d6a84dbf93dd00f3',
         clientSecret: '0c9c5fff9e132c7e85ccc08633629706acfc33e4',
