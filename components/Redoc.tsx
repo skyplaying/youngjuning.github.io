2adadcad10aa78bf6d0719ef133a696bc7dc872d
洛竹 <youngjuning@aliyun.com>
2021-08-02 15:48:28 +0800

    chore: remove components folder

--- components/Redoc.tsx
+++
@@ -1,6 +0,0 @@
-// yarn add core-js mobx redoc styled-components
-import React from 'react';
-import { RedocStandalone } from 'redoc';
-export default () => (
-  <RedocStandalone specUrl="http://petstore.swagger.io/v2/swagger.json"></RedocStandalone>
-);
